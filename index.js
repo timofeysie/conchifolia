81d9ff3218679be4d8863743788017717d214fb0
Timothy Curchod <timofeyc@hotmail.com>
2018-07-30 13:01:22 +1000

    Added get for angular dist dir

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const allowedExt = [
+  '.js',
+  '.ico',
+  '.css',
+  '.png',
+  '.jpg',
+  '.woff2',
+  '.woff',
+  '.ttf',
+  '.svg',
+];
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('*', (req, res) => {
+    if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+      res.sendFile(path.resolve(`views/my-dream-app/${req.url}`));
+    } else {
+      res.sendFile(path.resolve('views/my-dream-app/index.html'));
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
