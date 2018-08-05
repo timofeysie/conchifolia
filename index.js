f442d7a93d44bdc274137797a4e5566306c5d426
Timothy Curchod <timofeyc@hotmail.com>
2018-08-05 18:48:01 +1000

    Allow any host connection to GET

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ express()
   .use(bodyParser.json())
   .use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://https://radiant-springs-38893.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', false);
     // Pass to next layer of middleware
     next();})
   .set('views', path.join(__dirname, 'views'))
