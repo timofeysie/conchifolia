370b67a6a94e8dc778b350c29b4dfe17299a95ca
Timothy Curchod <timofeyc@hotmail.com>
2018-08-06 20:36:42 +1000

    reverted to previous function

--- index.js
+++ index.js
@@ -66,10 +66,10 @@ express()
       console.log('!OPTIONS');
       var headers = {};
       // IE8 does not allow domains to be specified, just the *
-      headers["Access-Control-Allow-Origin"] = req.headers.origin;
-      //headers["Access-Control-Allow-Origin"] = "*";
+      //headers["Access-Control-Allow-Origin"] = req.headers.origin;
+      headers["Access-Control-Allow-Origin"] = "*";
       headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-      headers["Access-Control-Allow-Credentials"] = false;
+      headers["Access-Control-Allow-Credentials"] = true;
       headers["Access-Control-Max-Age"] = '86400'; // 24 hours
       headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
       res.writeHead(200, headers);
